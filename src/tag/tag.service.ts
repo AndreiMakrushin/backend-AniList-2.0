797e58d4e02fafad651afa573078a86a19618d28
Andrei <andrei18makrushin@gmail.com>
2025-03-26 16:21:46 +0500

    feat: create table in data base

--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { TagEntity } from './tag.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class TagService {
-  findAll(): string[] {
-    return ['tag1', 'tag2', 'tag3'];
+  constructor(
+    @InjectRepository(TagEntity)
+    private readonly tagRepository: Repository<TagEntity>,
+  ) {}
+  async findAll(): Promise<TagEntity[]> {
+    return await this.tagRepository.find();
   }
 }
