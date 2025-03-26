4ea70d85fac1131d4d3dc0215e1b57c3a0e1843e
Andrei <andrei18makrushin@gmail.com>
2025-03-26 16:40:00 +0500

    feat: add method for tags

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -1,12 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Post } from '@nestjs/common';
 import { TagService } from './tag.service';
-import { TagEntity } from './tag.entity';
+
+interface Tag {
+  id: number;
+  name: string;
+}
 
 @Controller('tags')
 export class TagController {
   constructor(private readonly tagService: TagService) {}
   @Get()
-  async findAll(): Promise<TagEntity[]> {
-    return await this.tagService.findAll();
+  async findAll(): Promise<{ tags: Tag[] }> {
+    const tags = await this.tagService.findAll();
+    return {
+      tags: tags.map((tag) => {
+        return {
+          id: tag.id,
+          name: tag.name,
+        };
+      }),
+    };
+  }
+
+  @Post()
+  async create() {
+    return await this.tagService.create({ name: 'test' });
+  }
+  async update() {
+    return await this.tagService.update(1, { name: 'test' });
+  }
+
+  @Delete()
+  async delete() {
+    return await this.tagService.delete(1);
   }
 }
--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -12,4 +12,17 @@ export class TagService {
   async findAll(): Promise<TagEntity[]> {
     return await this.tagRepository.find();
   }
+
+  async create(tagData: Omit<TagEntity, 'id'>): Promise<TagEntity> {
+    const newTag = this.tagRepository.create(tagData);
+    return this.tagRepository.save(newTag);
+  }
+
+  async update(id: number, updateData: Partial<TagEntity>): Promise<TagEntity> {
+    await this.tagRepository.update(id, updateData);
+    return this.tagRepository.findOneBy({ id }) as Promise<TagEntity>;
+  }
+  async delete(id: number): Promise<void> {
+    await this.tagRepository.delete(id);
+  }
 }
